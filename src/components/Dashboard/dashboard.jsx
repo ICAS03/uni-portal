c319e6fdcf9a171435ca03e0bc1a97e8d8c0ea05
ICAS03 <111299269+ICAS03@users.noreply.github.com>
2024-07-22 21:49:08 +0800

    dashboard + firebase

--- src/components/Dashboard/dashboard.jsx
+++ src/components/Dashboard/dashboard.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Navbar from '../Navbar/navbar';
-
-
+import DashboardCard from './dashboardCard'
+import RadialBar from './radialbar';
+import AlertCard from './alertCard';
+import DashboardCalendar from './dashboardCalendar';
 const dashboard = () => {
   return (
     <>
     <Navbar></Navbar>
-    <div>
-    Dashboard
-    </div>
+     <h1>Welcome Back!</h1>
+        <DashboardCard module="Software Engineering" color="blue"/>
+    <DashboardCard module="System Architecture" color="purple"/>
+    <DashboardCard module="Database Structure" color="yellow"/>
+    <RadialBar score={75}/>
+    <h1>Deadlines</h1>
+    <DashboardCalendar/>
+    <h1>Alerts</h1>
+    <AlertCard alertdate="Monday, 29 July 2024" alertmodule="Ideating Start-Up" alertmessage="Good Afternoon Students, I would like to inform that your assignment has extended to 27/06"/>
     </>
   )
 }
